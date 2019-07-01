59a1a4bdfc47c05225c9911b4a20ff95fe7208b4
yuzurupurico <52344517+yuzurupurico@users.noreply.github.com>
2019-07-02 05:40:56 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,80 +1,32 @@
+# インストールした discord.py を読み込む
 import discord
 import random  # おみくじで使用
+from discord.ext import tasks
+from datetime import datetime 
+from discord.ext import commands
 
-client = discord.Client()  # 接続に使用するオブジェクト
+fincount    = 0         # 凸終了人数カウント変数を初期値0で定義する(ここで宣言するとグローバルになって各処理から参照できます)
+boss        = ["ゴブリングレート","ライライ","シードレイク","ネプテリオン","カルキノス"]
+bossindex   = 0
 
-fincount = 0  # 凸終了人数カウント変数を初期値0で定義する(ここで宣言するとグローバルになって各処理から参照できます)
+# 自分のBotのアクセストークンに置き換えてください
+TOKEN       = 
+CHANNEL_R   =     # 予約チャンネルID
+CHANNEL_T   =     # 凸報告チャンネルID
+CHANNEL_S   =     # 集計チャンネルID
+roles_mem   =     # 役職くらめんのID
 
-# 30人分のユーザーの名前リストとIDリスト（discord上で右クリック取得）を作成する
-membername = [  # 下のＩＤと同じ並びで設定する。名前をidから動的に取得できれば名前リストは不要です。
-"トモ",
-"マツリ",
-"オウカ",
-"ゆずる",
-"クウカ",
-"ノウェム",
-"リマ",
-"しずる",
-"ばずる",
-"じーた",
-"からす",
-"きつね",
-"てんぷら",
-"べーこん",
-"ご空",
-"ちゃおず",
-"くりりん",
-"桜木",
-"やべ",
-"かんがえるのが",
-"面倒",
-"になって",
-"きました",
-"とにかく",
-"でばっぐ",
-"ように",
-"うめれば",
-"おーけー",
-"ですすｓ",
-"最後の戦士",
-]
+# user情報リスト
+memberid    = []            # クラメンのＩＤを取得するリスト
+membername  = []            # クラメンの名前を取得するリスト
+usercount   = 0             # クラメンの人数
+totsucount  = []            # 凸数リスト
 
-memberid = [    # メンバーのdiscordユーザーidリスト。上の名前リストと順番を合わせる
-0,                  # "トモ"のＩＤ
-1,                  # "マツリ",のＩＤ
-2,                  # "オウカ",のＩＤ
-572012584241201153, # "ゆずる",のＩＤ
-4,                  # "クウカ",のＩＤ
-5,                  # "ノウェム",のＩＤ
-6,                  # "リマ",のＩＤ
-7,                  # "しずる",のＩＤ
-8,                  # "ばずる",のＩＤ
-9,                  # "じーた",のＩＤ
-10,                 # "からす",のＩＤ
-11,                 # "きつね",のＩＤ
-12,                 # "てんぷら",のＩＤ
-13,                 # "べーこん",のＩＤ
-14,                 # "ご空",のＩＤ
-15,                 # "ちゃおず",のＩＤ
-16,                 # "くりりん",のＩＤ
-17,                 # "桜木",のＩＤ
-18,                 # "やべ",のＩＤ
-19,                 # "かんがえるのが",のＩＤ
-20,                 # "面倒",のＩＤ
-21,                 # "になって",のＩＤ
-22,                 # "きました",のＩＤ
-23,                 # "とにかく",のＩＤ
-24,                 # "でばっぐ",のＩＤ
-25,                 # "ように",のＩＤ
-26,                 # "うめれば",のＩＤ
-27,                 # "おーけー",のＩＤ
-28,                 # "ですすｓ",のＩＤ
-29 ]                # "最後の戦士",のＩＤ
+# 接続に必要なオブジェクトを生成
+client = discord.Client()
 
 
-# 30人分の凸数リストを初期値=0で作成する
-totsucount = [0] * 30
-
+# 起動時に動作する処理
 @client.event
 async def on_ready():
     """起動時に通知してくれる処理"""
@@ -84,75 +36,227 @@ async def on_ready():
     print(discord.__version__)  # discord.pyのバージョン
     print('------')
 
+# メッセージ受信時に動作する処理
 @client.event
 async def on_message(message):
-    global fincount     # グローバルで定義した変数を使用する宣言
-    global cname        # グローバルで定義した変数を使用する宣言
-    global totsucount   # グローバルで定義した変数を使用する宣言
-    """メッセージを処理"""
-    if message.author.bot:  # ボットのメッセージをハネる
+    global fincount
+    global totsucoun
+    global boss
+    global TOKEN
+    global CHANNEL_R
+    global CHANNEL_T
+    global roles_mem
+    global memberid
+    global membername
+    global usercount
+
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
         return
+    # 「/neko」と発言したら「にゃーん」が返る処理
+    if message.content == '/neko':
+        await message.channel.send('に”ゃ"あ”あ”あ”あ”あ”ん”！')
+    
+    # クランメンバー（役職：くらめん）の情報を自動取得して更新する ※メンバーに変化あったら一回実行が必要
+    if message.content == '/member':
+        usercount   = 0
+        memberid    = []            # クラメンのＩＤリストを初期化する
+        membername  = []            # クラメンの名前リストを初期化する
+        totsucount  = []            # 凸数リストを初期化する
+        for member in message.guild.members:    # user情報を全員チェックする
+            print( member.name )
+            for role in member.roles:           # userのロールをチェックする
+                if role.id == roles_mem:        # ロールが「くらめん」なら
+                    membername.append(member.name)
+                    memberid.append(member.id)
+                    totsucount.append(0)
+                    usercount += 1              # user数をカウントアップする
+        print(membername)
+        print(memberid)
+        print(totsucount)
+        print(usercount)
 
-    if message.content == "!眠たい":
-        # チャンネルへメッセージを送信
-        await message.channel.send(f"{message.author.mention}さん 寝ましょう")  # f文字列（フォーマット済み文字列リテラル）
+    
+    if message.content == '/obuse':
+        await message.channel.send('0:40 クスリ')
+    
+    if message.content == '/inu':
+        await message.channel.send('お゛お゛お゛お゛お゛お゛ん゛!')
+            
+    if message.content == '/yaaman':
+        await message.channel.send('ダメです')
 
-    elif message.content == "凸開始":
-        # 開始報告した人を名前リストから探し、凸数を更新する
-        for i in range(30):                             # i=0からi=29まで30回繰り返す処理を実行する
-            if memberid[i] == message.author.id:        # 報告者とidが一致したら
-                await message.channel.send(f"{message.author.mention}さん " + str(totsucount[i]+1) + "凸開始です。" )
+    if message.content == "/ダメです":
 
-    elif message.content == "凸終了":
-        # 終了報告した人を名前リストから探し、凸数を更新する
-        for i in range(30):                             # i=0からi=29まで30回繰り返す処理を実行する
-            if memberid[i] == message.author.id:        # 報告者とidが一致したら
-                totsucount[i] += 1                      # 凸回数をカウントアップする
-                if totsucount[i] == 3:                  # 3なら終わりメッセージも追加
-                    fincount += 1                       # fincount（凸終了人数）をカウントアップ（+1）する
-                    await message.channel.send(f"{message.author.mention}さん " + str(totsucount[i]) + "凸終了です。お疲れ様です！ 凸終わり"+ str(fincount)+ "人目" )
-                elif totsucount[i] > 3:                 # 3より大きければエラーメッセージ表示
-                    await message.channel.send(f"{message.author.mention}さん 凸報告多すぎですよ" )
-                else:                                   # 3より小さいとき
-                    await message.channel.send(f"{message.author.mention}さん " + str(totsucount[i]) + "凸終了です。" )
-
-    elif message.content == "!凸残り":  # 凸残ってる人だけ表示する場合
-        tcount = 0
-        for i in range(30):                             # i=0からi=29まで30回繰り返す処理を実行する
-            if totsucount[i] < 3:                       # 凸回数3回以下なら残り凸回数を表示する
-                await message.channel.send(membername[i] + "さん 残り" + str(3-totsucount[i]) + "凸です。" )
-                tcount += 1                             #残り人数をカウントアップする
-        await message.channel.send("以上 残り" + str(tcount) + "人です。" )
-
-    elif message.content == "!凸状況":  # 全員の凸状況を把握したいとき
-        for i in range(30):                             # i=0からi=29まで30回繰り返す処理を実行する
-            await message.channel.send(membername[i] + "さんの現在までの凸完了回数＝" + str(totsucount[i]) + "回です。" )
-
-    elif message.content == "!投票":
-        # リアクションアイコンを付けたい
-        q = await message.channel.send("あなたは右利きですか？")
-        [await q.add_reaction(i) for i in ('⭕', '❌')]  # for文の内包表記
-
+        await message.channel.send(f"（ﾌﾞﾘﾌﾞﾘﾌﾞﾘﾌﾞﾘｭﾘｭﾘｭﾘｭﾘｭﾘｭ！！！！！！ﾌﾞﾂﾁﾁﾌﾞﾌﾞﾌﾞﾁﾁﾁﾁﾌﾞﾘﾘｲﾘﾌﾞﾌﾞﾌﾞﾌﾞｩｩｩｩｯｯｯ")
+                        
     elif message.content == "!おみくじ":
         # Embedを使ったメッセージ送信 と ランダムで要素を選択
         embed = discord.Embed(title="おみくじ", description=f"{message.author.mention}さんの今日の運勢は！",
                               color=0x2ECC69)
         embed.set_thumbnail(url=message.author.avatar_url)
-        embed.add_field(name="[運勢] ", value=random.choice(('大吉', '吉', '凶', '大凶')), inline=False)
-        await message.channel.send(embed=embed)
+        embed.add_field(name="[運勢] ", value=random.choice(('大吉', '中吉','吉', '小吉', '凶', '大凶')), inline=False)
+        await message.channel.send(embed=embed) 
 
-    elif message.content == "!ダイレクトメッセージ":
-        # ダイレクトメッセージ送信
-        dm = await message.author.create_dm()
-        await dm.send(f"{message.author.mention}さんにダイレクトメッセージ")
+    elif message.content == "!hori":
+        # Embedを使ったメッセージ送信 と ランダムで要素を選択
+        embed = discord.Embed(title="hori", description=f"ほりくんの迷言集",
+                              color=0x2ECC69)
+        embed.set_thumbnail(url=message.author.avatar_url)
+        embed.add_field(name="[hori] ", value=random.choice(('ぬそぉ...', 'ちんちんケルベロス', 'ムラムラ','ケルベロス掘りします')), inline=False)
+        await message.channel.send(embed=embed) 
 
-    elif message.content == "!時間":
-        # ダイレクトメッセージ送信
-        now = datetime.now().strftime('%H:%M')  # 現在の時刻を取得する
-        await message.channel.send(now)  # f文字列（フォーマット済み文字列リテラル）
+        
+    elif message.content == "!brain":
+        # Embedを使ったメッセージ送信 と ランダムで要素を選択
+        embed = discord.Embed(title="brain", description=f"あなたは何ぶれ？",
+                              color=0x2ECC69)
+        embed.set_thumbnail(url=message.author.avatar_url)
+        embed.add_field(name="[brain] ", value=random.choice(('がばいん','ぶれぶれいん','上ぶれいん', '下ぶれいん', '神ぶれいん', '凶ぶれいん')), inline=False)
+        await message.channel.send(embed=embed) 
 
-# botの接続と起動
-# （botアカウントのアクセストークンを入れてください）
-client.run("ここにトークンを打ち込む")
+    elif message.content == "!toroy":
+        # Embedを使ったメッセージ送信 と ランダムで要素を選択
+        embed = discord.Embed(title="toroy", description=f"がばいさんかとろいさんか",
+                              color=0x2ECC69)
+        embed.set_thumbnail(url=message.author.avatar_url)
+        embed.add_field(name="[toroy] ", value=random.choice(('がばいです','呼ばれて飛び出てとろいさん','寝落ちしたわ', '寝言行くぜ！')), inline=False)
+        await message.channel.send(embed=embed) 
 
-# py C:\Users\owner\Desktop\BOT\bot.py
+    elif message.content == "凸開始":
+        # 開始報告した人を名前リストから探し、凸数を更新する
+        for i in range(usercount):                      # i=0からi=29まで30回繰り返す処理を実行する
+            if memberid[i] == message.author.id:        # 報告者とidが一致したら
+                channel = client.get_channel(CHANNEL_T)
+                await channel.send(f"{message.author.mention}さん " + str(totsucount[i]+1) + "凸開始です。" )
+
+    elif message.content == "凸終了":
+        # 終了報告した人を名前リストから探し、凸数を更新する
+        channel = client.get_channel(CHANNEL_T)
+        for i in range(usercount):                      # i=0からi=29まで30回繰り返す処理を実行する
+            if memberid[i] == message.author.id:        # 報告者とidが一致したら
+                totsucount[i] += 1                      # 凸回数をカウントアップする
+                if totsucount[i] == 3:                  # 3なら終わりメッセージも追加
+                    fincount += 1                       # fincount（凸終了人数）をカウントアップ（+1）する
+                    await channel.send(f"{message.author.mention}さん " + str(totsucount[i]) + "凸終了です。お疲れ様です！ 凸終わり"+ str(fincount)+ "人目" )
+                elif totsucount[i] > 3:                 # 3より大きければエラーメッセージ表示
+                    await channel.send(f"{message.author.mention}さん 凸報告多すぎですよ" )
+                else:                                   # 3より小さいとき
+                    await channel.send(f"{message.author.mention}さん " + str(totsucount[i]) + "凸終了です。" )
+
+    elif message.content == "!凸残り":  # 凸残ってる人だけ表示する場合
+        tmessage = ""
+        tcount = 0
+        for i in range(usercount):                      # i=0からi=29まで30回繰り返す処理を実行する
+            if totsucount[i] < 3:                       # 凸回数3回以下なら残り凸回数を表示する
+                tmessage = tmessage + membername[i] + "さん 残り" + str(3-totsucount[i]) + "凸です。\n"
+                tcount += 1                             #残り人数をカウントアップする
+        channel = client.get_channel(CHANNEL_S)
+        await channel.send(tmessage + "以上 残り" + str(tcount) + "人です。" )
+
+    elif message.content == "!凸状況":  # 全員の凸状況を把握したいとき
+        tempmessage = ""
+        for i in range(usercount):      # i=0からi=29まで30回繰り返す処理を実行する
+            tempmessage = tempmessage + membername[i] + "さんの現在までの凸完了回数 = " + str(totsucount[i]) + "回です。" + "\n"
+        channel = client.get_channel(CHANNEL_S)
+        await channel.send( tempmessage )
+
+    if message.content == "1凸":
+        
+        channel = client.get_channel(CHANNEL_T)
+        await channel.send(f"{message.author.mention}さんの1凸目です") 
+
+    if message.content == "2凸":
+        
+        channel = client.get_channel(CHANNEL_T)
+        await channel.send(f"{message.author.mention}さんの2凸目です")    
+
+    if message.content == "3凸":
+        
+        channel = client.get_channel(CHANNEL_T)
+        await channel.send(f"{message.author.mention}さんの3凸目です")
+
+    if message.content == "/タスキル":
+        
+        channel = client.get_channel(593482786581643264)
+        await channel.send(f"{message.author.mention}さんがタスキルしました")         
+        
+    if message.content == "持ち越し":
+
+        channel = client.get_channel(CHANNEL_T)
+        await channel.send(f"{message.author.mention}さんの持ち越しです")   
+
+    if message.content == "1凸LA":
+        
+        channel = client.get_channel(CHANNEL_T)
+        await channel.send(f"{message.author.mention}さんの1凸目のLAです")
+
+    if message.content == "2凸LA":
+        
+        channel = client.get_channel(CHANNEL_T)
+        await channel.send(f"{message.author.mention}さんの2凸目のLAです")        
+
+    if message.content == "3凸LA":
+        
+        channel = client.get_channel(CHANNEL_T)
+        await channel.send(f"{message.author.mention}さんの3凸目のLAです")   
+
+    if message.content == "/1st":
+        bossindex = 0
+        channel = client.get_channel(CHANNEL_T)
+        await channel.send(f"<@&{roles_mem}>" + boss[bossindex])
+        
+    if message.content == "/2nd":
+        bossindex = 1
+        channel = client.get_channel(CHANNEL_T)
+        await channel.send(f"<@&{roles_mem}>" + boss[bossindex])
+
+    if message.content == "/3rd":
+        bossindex = 2
+        channel = client.get_channel(CHANNEL_T)
+        await channel.send(f"<@&{roles_mem}>" + boss[bossindex])
+        
+    if message.content == "/4th":
+        bossindex = 3
+        channel = client.get_channel(CHANNEL_T)
+        await channel.send(f"<@&{roles_mem}>" + boss[bossindex])
+        
+    if message.content == "/5th":
+        bossindex = 4
+        channel = client.get_channel(CHANNEL_T)
+        await channel.send(f"<@&{roles_mem}>" + boss[bossindex])
+        
+    if message.content == "/助けて":
+        
+        channel = client.get_channel(CHANNEL_T)
+        await channel.send(f"<@&{roles_mem}> 助けてマリオ！")    
+        
+
+# ボスに行きたい人を募集するとき
+
+# 50秒に一回ループ
+@tasks.loop(seconds=50)
+async def loop():
+    global fincount
+    global totsucount
+
+    # 現在の時刻
+    now = datetime.now().strftime('%H:%M')
+    if now == '19:00':
+        channel = client.get_channel(560495611280097295)
+        await channel.send('プリコネの更新1時間前だよ～')  
+
+    elif now == '20:00':
+        channel = client.get_channel(560495611280097295)
+        await channel.send('プリコネの更新時間だよ～')  
+        channel = client.get_channel(584441360506028213)
+        await channel.send('今日のクラバトの集計です')  
+        fincount = 0
+        totsucount = [0] * usercount
+
+#ループ処理実行
+loop.start()
+
+
+# Botの起動とDiscordサーバーへの接続
+client.run( TOKEN )
